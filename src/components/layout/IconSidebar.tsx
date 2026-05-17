9e30ea7128173c7038b0d56fe2c4b412861575e3
masumbillah360 <masumbillah6584@gmail.com>
2026-05-18 04:44:18 +0600

    feat: fix the icon color in sepia mode for the icons sidebar

--- src/components/layout/IconSidebar.tsx
+++ src/components/layout/IconSidebar.tsx
@@ -1,8 +1,9 @@
 'use client';
 import Link from 'next/link';
-
+import { useApp } from '@/context/AppContext';
 import {
     QuranLogo,
+    QuranLogoSepia,
     IcoHome,
     IcoReadQuran,
     IcoGoToAyah,
@@ -19,6 +20,23 @@ const NAV_ITEMS = [
 ];
 
 export default function IconSidebar() {
+    const {
+        activeIconTab,
+        setActiveIconTab,
+        setIsSurahSidebarOpen,
+        isSurahSidebarOpen,
+        resolvedTheme,
+    } = useApp();
+
+    const handleClick = (id: string) => {
+        if (activeIconTab === id) {
+            setIsSurahSidebarOpen(!isSurahSidebarOpen);
+        } else {
+            setActiveIconTab(id);
+            setIsSurahSidebarOpen(true);
+        }
+    };
+
     return (
         <aside className="hidden md:flex flex-col items-center w-16 shrink-0 bg-(--bg-accent)/5  py-4 gap-6 h-full z-30">
             {/* Brand / Logo icon at top */}
@@ -26,19 +44,25 @@ export default function IconSidebar() {
                 href="/"
                 aria-label="Home"
                 className="transition-transform duration-200 hover:scale-105">
-                <QuranLogo size={36} />
+                {resolvedTheme === 'sepia' ? (
+                    <QuranLogoSepia size={36} />
+                ) : (
+                        <QuranLogo size={36} />
+                )}
             </Link>
 
             {/* Navigation item column - centered vertically */}
             <div className="flex flex-col items-center gap-5 w-full flex-1 justify-center">
                 {NAV_ITEMS.map(({ id, icon: Icon, label }) => {
-                    const isActive = true;
+                    const isActive = activeIconTab === id;
                     return (
                         <button
                             key={id}
-                            onClick={() => { }}
+                            onClick={() => handleClick(id)}
+                            // title={label}
                             className={`relative size-10 rounded-xl flex items-center justify-center transition-all duration-300 group cursor-pointer
-                ${isActive
+                ${
+                                isActive
                                     ? 'bg-[#1b8043]/10 text-[#1b8043] font-semibold scale-105'
                                     : 'text-gray-400 hover:bg-(--bg-surface) hover:text-gray-600'
                                 }`}>
