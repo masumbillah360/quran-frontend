64519a66ce29c1365333d7325e2cdb729bc923b4
masumbillah360 <masumbillah6584@gmail.com>
2026-05-18 01:23:39 +0600

    feat: copied the css and icons

---
+++ src/assets/icons/index.ts
@@ -0,0 +1,6 @@
+export * from './QuranLogo';
+export * from './IcoHome';
+export * from './IcoReadQuran';
+export * from './IcoGoToAyah';
+export * from './IcoBookmark';
+export * from './IcoOthers';
